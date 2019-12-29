1b76b19549c33f9dce07071fcbb81121b04bc6cb
John Rush <john@rush.cool>
2019-12-29 10:05:21 -0800

    day 6 and formatting

--- src/adventofcode/day6.clj
+++ src/adventofcode/day6.clj
@@ -1,5 +1,8 @@
 (ns adventofcode.day6
-  (:require [adventofcode.utils :as utils]))
+  (:require
+    [adventofcode.utils :as utils]
+    [clojure.set :refer :all]))
+
 
 (defn update-children!
   [orbit-atom parent-id]
@@ -7,50 +10,81 @@
     (doseq [child-id (:children (get @orbit-atom parent-id))
             :let [child (get @orbit-atom child-id)]]
       (swap! orbit-atom
-             assoc 
+             assoc
              child-id
-             {:depth (+ 1 (:depth parent)) 
+             {:depth (+ 1 (:depth parent))
               :parent (:parent child)
               :children (:children child)})
       (update-children! orbit-atom child-id))))
 
-(defn count-orbits
+
+(defn build-graph
   [orbit-pairs]
   (let [a (atom {})]
-  (doseq [orb orbit-pairs
-          :let [parent (get @a (first orb))
-                current (get @a (second orb))]]
-    (if parent
-      (swap! a 
-             assoc 
-             (first orb) 
-             {:depth (:depth parent)
-              :parent (:parent parent)
-              :children (conj (:children parent) (second orb))})
-      (swap! a 
-             assoc 
-             (first orb) 
-             {:depth 0 
-              :parent nil 
-              :children #{(second orb)}}))
-    (if current 
-      (doall 
-        (swap! a 
-               assoc 
-               (second orb) 
-               {:depth (if parent (+ 1 (:depth parent)) 1) 
+    (doseq [orb orbit-pairs
+            :let [parent (get @a (first orb))
+                  current (get @a (second orb))]]
+      (if parent
+        (swap! a
+               assoc
+               (first orb)
+               {:depth (:depth parent)
+                :parent (:parent parent)
+                :children (conj (:children parent) (second orb))})
+        (swap! a
+               assoc
+               (first orb)
+               {:depth 0
+                :parent nil
+                :children #{(second orb)}}))
+      (if current
+        (doall
+          (swap! a
+                 assoc
+                 (second orb)
+                 {:depth (if parent (+ 1 (:depth parent)) 1)
+                  :parent (first orb)
+                  :children (:children current)})
+          (update-children! a (second orb)))
+        (swap! a
+               assoc
+               (second orb)
+               {:depth (if parent (+ 1 (:depth parent)) 1)
                 :parent (first orb)
-                :children (:children current)})
-        (update-children! a (second orb)))
-      (swap! a 
-             assoc 
-             (second orb)
-             {:depth (if parent (+ 1 (:depth parent)) 1) 
-              :parent (first orb)
-              :children #{}}))
-
-  )
-  (apply + (map :depth (vals @a)))
-  ))
+                :children #{}})))
+    @a))
 
 
+(defn orbits
+  [orbit-graph pos-id]
+  (let [parent-id (:parent (get orbit-graph pos-id))]
+    (if parent-id
+      (conj (orbits orbit-graph parent-id) pos-id)
+      [pos-id])))
+
+
+(defn extrasection
+  [& ss]
+  (difference
+    (union ss)
+    (intersection ss)))
+
+
+(defn common-orbits
+  [orbit-graph first-pos second-pos]
+  (let [first-orbits (set (orbits orbit-graph first-pos))
+        second-orbits (set (orbits orbit-graph second-pos))]
+    (difference
+      (union first-orbits second-orbits)
+      (intersection first-orbits second-orbits))))
+
+
+(defn orbital-transfers
+  [orbit-graph first-pos second-pos]
+  (- (count (common-orbits orbit-graph first-pos second-pos)) 2)) ;; -2 because we don't want the YOU/SAN to count as an orbit
+
+
+
+(defn count-orbits
+  [orbit-pairs]
+  (apply + (map :depth (vals (build-graph orbit-pairs)))))

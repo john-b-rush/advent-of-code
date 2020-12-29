e3f5a9e6f290b1f236dd12ae0483f9f0e550ca04
John Rush <jbr@john-rush.com>
2020-12-29 11:18:26 -0800

    day18

--- 2020/src/advent_of_code/day18.clj
+++ 2020/src/advent_of_code/day18.clj
@@ -3,30 +3,161 @@
     [clojure.string :as str]))
 
 
+(defn new-math
+  [string idx]
+  (loop [idx idx
+         running-value 0
+         op +]
+    (if (= (count string) idx)
+      [running-value idx]
+      (let [value (nth string idx)]
+        (case value
+          \+
+          (recur (inc idx) running-value +)
 
-;102         total 0
-;103         operation +
-;104         len 0]
-;105      (let [item (first items)]
-;106     (case item
-;107
-;108          nil
-;109          [total len]
-;110
-;111      ")"
-;112      [total (+ 2 len)]
-;113
-;114       "+"
-;115       (recur (rest items) total + (inc len))
-;116
-;117       "*"
-;118       (recur (rest items) total * (inc len))
-;119
-;120       "("
-;121       (let [[sub-total sub-len] (new-math (drop (inc len) items))]
-;122         (print ":" (vec (drop (inc len) items))"\n")
-;123         (print "xx" total sub-len "\n\n")
-;124         (recur (drop sub-len items) (+ total sub-total) nil (+ len sub-len)))
-;125
-;126
-;127       (recur (rest items) (operation total (read-string item)) nil (inc len))))))
+          \*
+          (recur (inc idx) running-value *)
+
+          \(
+          (let [[sub-value offset] (new-math string (inc idx))]
+            (recur
+              (inc offset)
+              (op running-value sub-value)
+              nil))
+
+          \)
+          [running-value idx]
+
+          (recur (inc idx) (op running-value (read-string (str value))) nil))))))
+
+
+(defn process-line
+  [line]
+  (-> line
+      (str/replace #"\s+" "")
+      (new-math 0)
+      (first)))
+
+
+;;Part 1
+(->> "inputs/day18.txt"
+     (slurp)
+     (str/split-lines)
+     (map process-line)
+     (reduce +))
+
+
+(defn line-ops
+  [line]
+  (reduce
+    (fn [acc item]
+      (case item
+        \+
+        (conj acc +)
+        \*
+        (conj acc *)
+        \space
+        acc
+        \(
+        (conj acc \()
+        \)
+        (conj acc \))
+        (conj acc (read-string (str item)))))
+    []
+    line))
+
+
+(declare adv-math+ adv-math* parens)
+
+
+(defn- adv-math
+  [idx items]
+  (print "adm" idx items "\n")
+  (->>
+    items
+    (parens idx)
+    (adv-math+)
+    (adv-math*)))
+
+
+(defn- parens
+  [idx items]
+  (print idx items "\n")
+  (loop [new-items []
+         idx idx]
+    (if (= (count items) idx)
+      [idx new-items]
+      (let [value (nth items idx)]
+        (case value
+          \(
+          (let [[offset [sub-value]] (adv-math (inc idx) items)]
+            (print sub-value offset)
+            (recur
+              (conj new-items sub-value)
+              (inc offset)))
+
+          \)
+          [idx new-items]
+
+          (recur (conj new-items value) (inc idx)))))))
+
+
+(defn- adv-math+
+  [[idx items]]
+  (print "pls" idx items "\n")
+  [idx
+   (->>
+     items
+     (reduce
+       (fn [[acc prev] item]
+         (cond
+           (= + prev)
+           [(conj
+              (subvec acc
+                      0 (- (count acc) 2))
+              (+ (nth acc (- (count acc) 2))
+                 item))
+            item]
+
+           :else
+           [(conj acc item) item]))
+       [[] nil])
+     (first))])
+
+
+(defn- adv-math*
+  [[idx items]]
+  [idx
+   (->>
+     items
+     (reduce
+       (fn [[acc prev] item]
+         (cond
+           (= * prev)
+           [(conj
+              (subvec acc
+                      0 (- (count acc) 2))
+              (* (nth acc (- (count acc) 2))
+                 item))
+            item]
+
+           :else
+           [(conj acc item) item]))
+       [[] nil])
+     (first))])
+
+
+(defn run-line
+  [line]
+  (->> line
+       (line-ops)
+       (adv-math 0)
+       (second)
+       (first)))
+
+;;Part 2
+(->> "inputs/day18.txt"
+     (slurp)
+     (str/split-lines)
+     (map run-line)
+     (reduce +))
---
+++ 2020/src/advent_of_code/day25.clj
@@ -0,0 +1,7 @@
+(ns advent-of-code.day25
+  (:require
+    [clojure.math.combinatorics :as combo]
+    [clojure.string :as str]))
+
+;14222596
+;4057428

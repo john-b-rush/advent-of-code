38a9f04aa0ec18ff13ebaba1a679158fed120cf3
John Rush <jbr@john-rush.com>
2020-12-31 09:52:44 -0800

    day25

--- 2020/src/advent_of_code/day25.clj
+++ 2020/src/advent_of_code/day25.clj
@@ -3,5 +3,24 @@
     [clojure.math.combinatorics :as combo]
     [clojure.string :as str]))
 
-;14222596
-;4057428
+;; Inputs:
+;; 14222596
+;; 4057428
+
+;; loop size = 3616052
+#_(loop [size 0
+       value 1]
+  (if (= 14222596 value)
+    size
+    (recur 
+      (inc size)
+      (rem (* 7 value) 20201227))))
+
+;; Key = 3286137
+(loop [size 0
+       value 1]
+  (if (or (= 3616052 size))
+    value
+    (recur 
+      (inc size)
+      (rem (* 4057428 value) 20201227))))
